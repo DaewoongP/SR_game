22c57837d2e261990e71346b8b793df654d9fcca
shortusernam <wjdghks718@gmail.com>
2023-03-31 15:09:42 +0900

    ImguiTool
    누수체크, imgui 툴 순서 수정됨

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -85,6 +85,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     bool show_another_window = true;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
   
+
+
 	FAILED_CHECK_RETURN(Engine::Ready_Timer(L"Timer_Immediate"), FALSE);
 	FAILED_CHECK_RETURN(Engine::Ready_Timer(L"Timer_FPS60"), FALSE);
 
@@ -120,74 +122,75 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 				pMainApp->Update_MainApp(fTimer_FPS60);
 				pMainApp->LateUpdate_MainApp();
-				pMainApp->Render_MainApp();
-              
+                pMainApp->Render_MainApp();
+
+               // //
+               // ImGui_ImplDX9_NewFrame();
+               // ImGui_ImplWin32_NewFrame();
+               // ImGui::NewFrame();
+
+               // //// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+               // if (show_demo_window)
+               //     ImGui::ShowDemoWindow(&show_demo_window);
+
+               //// //// 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+               // {
+               //     static float f = 0.0f;
+               //     static int counter = 0;
+               //     ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
                //
-                ImGui_ImplDX9_NewFrame();
-                ImGui_ImplWin32_NewFrame();
-                ImGui::NewFrame();
+               //    ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+               //    ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+               //    ImGui::Checkbox("Another Window", &show_another_window);
 
-                //// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-                if (show_demo_window)
-                    ImGui::ShowDemoWindow(&show_demo_window);
+               //    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+               //    ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 
-               // //// 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
-                {
-                    static float f = 0.0f;
-                    static int counter = 0;
-                    ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-               
-                   ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-                   ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-                   ImGui::Checkbox("Another Window", &show_another_window);
+               //    if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+               //        counter++;
+               //    ImGui::SameLine();
+               //    ImGui::Text("counter = %d", counter);
+               //  
+               //    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+               //    ImGui::End();
+               //}
 
-                   ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-                   ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-                   if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                       counter++;
-                   ImGui::SameLine();
-                   ImGui::Text("counter = %d", counter);
-                 
-                   ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-                   ImGui::End();
-               }
-
-               //// 3. Show another simple window.
-               if (show_another_window)
-               {
-                    ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-                    ImGui::Text("Hello from another window!");
-                    if (ImGui::Button("Close Me"))
-                        show_another_window = false;
-                    ImGui::End();
-                }
+               ////// 3. Show another simple window.
+               //if (show_another_window)
+               //{
+               //     ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+               //     ImGui::Text("Hello from another window!");
+               //     if (ImGui::Button("Close Me"))
+               //         show_another_window = false;
+               //     ImGui::End();
+               // }
                
                 //// Rendering
-                ImGui::EndFrame();
-                pd3dDevice->SetRenderState(D3DRS_ZENABLE, FALSE);
-                pd3dDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
-                pd3dDevice->SetRenderState(D3DRS_SCISSORTESTENABLE, FALSE);
-                D3DCOLOR clear_col_dx = D3DCOLOR_RGBA((int)(clear_color.x * clear_color.w * 255.0f), (int)(clear_color.y * clear_color.w * 255.0f), (int)(clear_color.z * clear_color.w * 255.0f), (int)(clear_color.w * 255.0f));
-                pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, clear_col_dx, 1.0f, 0);
-                if (pd3dDevice->BeginScene() >= 0)
-                {
-                    ImGui::Render();
-                    ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
-                    pd3dDevice->EndScene();
-                }
-                HRESULT result = pd3dDevice->Present(NULL, NULL, NULL, NULL);
+                //ImGui::EndFrame();
+                //pd3dDevice->SetRenderState(D3DRS_ZENABLE, FALSE);
+                //pd3dDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
+                //pd3dDevice->SetRenderState(D3DRS_SCISSORTESTENABLE, FALSE);
+                //D3DCOLOR clear_col_dx = D3DCOLOR_RGBA((int)(clear_color.x * clear_color.w * 255.0f), (int)(clear_color.y * clear_color.w * 255.0f), (int)(clear_color.z * clear_color.w * 255.0f), (int)(clear_color.w * 255.0f));
+                ////pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, clear_col_dx, 1.0f, 0);
+                //if (pd3dDevice->BeginScene() >= 0)
+                //{
+                //    ImGui::Render();
+                //    ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+                //    pd3dDevice->EndScene();
+                //}
 
-              
+                //HRESULT result = pd3dDevice->Present(NULL, NULL, NULL, NULL);
+
+
                
 			} 
 			
 		}        
     }
 
-   /* ImGui_ImplDX9_Shutdown();
+    ImGui_ImplDX9_Shutdown();
     ImGui_ImplWin32_Shutdown();
-    ImGui::DestroyContext();*/
+    ImGui::DestroyContext();
 	_ulong dwRefCnt = 0;
 
 	if (dwRefCnt = Safe_Release(pMainApp))
