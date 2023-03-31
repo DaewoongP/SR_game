a7924c41d70797dbc156535a7f8b82dd9cecfeeb
AnCheolMin <94519819+AnCheolMin@users.noreply.github.com>
2023-03-31 14:14:11 +0900

    맵만 깔아둠
    맵만 깔아둠
    
    임시저장
    
    맵만 깔아둠
    
    맵만 깔아둠

---
+++ Client/Cube.cpp
@@ -0,0 +1,109 @@
+#include "stdafx.h"
+#include "Cube.h"
+
+#include "Export_Function.h"
+CCube::CCube(LPDIRECT3DDEVICE9 pGraphicDev)
+	:CGameObject(pGraphicDev)
+{
+}
+
+CCube::~CCube()
+{
+}
+
+HRESULT CCube::Ready_GameObject(void)
+{
+	FAILED_CHECK_RETURN(Add_Component(), E_FAIL);
+
+	m_pTransform->m_vScale = { 1.f, 1.f, 1.f };
+	//m_pTransform->m_vInfo[INFO_POS] = _vec3(0.f, 0.f, 10.f);
+
+	return S_OK;
+}
+
+_int CCube::Update_GameObject(const _float & fTimeDelta)
+{
+	_matrix		matCamWorld;
+
+	m_pGraphicDev->GetTransform(D3DTS_VIEW, &matCamWorld);
+	D3DXMatrixInverse(&matCamWorld, 0, &matCamWorld);
+
+	//m_pTransform->Set_Pos(matCamWorld._41, matCamWorld._42 + 3.f, matCamWorld._43);
+
+	__super::Update_GameObject(fTimeDelta);
+
+	Engine::Add_RenderGroup(RENDER_PRIORITY, this);
+
+	return 0;
+}
+
+void CCube::LateUpdate_GameObject(void)
+{
+	__super::LateUpdate_GameObject();
+}
+
+void CCube::Render_GameObject(void)
+{
+	m_pGraphicDev->SetTransform(D3DTS_WORLD, m_pTransform->Get_WorldMatrixPointer());
+
+	//m_pGraphicDev->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);
+	//m_pGraphicDev->SetRenderState(D3DRS_ZWRITEENABLE, FALSE);
+
+	m_pTextureCom->Set_Texture();
+
+	m_pBufferCom->Render_Buffer();
+
+	m_pGraphicDev->SetRenderState(D3DRS_ZWRITEENABLE, TRUE);
+	m_pGraphicDev->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);
+
+	__super::Render_GameObject();
+}
+
+HRESULT CCube::Add_Component(void)
+{
+	CComponent*		pComponent = nullptr;
+
+	pComponent = m_pBufferCom = dynamic_cast<CCubeTex*>(Engine::Clone_Proto(L"CubeTex", this));
+	NULL_CHECK_RETURN(m_pBufferCom, E_FAIL);
+	m_uMapComponent[ID_STATIC].insert({ L"CubeTex", pComponent });
+
+	pComponent = m_pRigid = dynamic_cast<CRigidbody*>(Engine::Clone_Proto(L"Rigidbody", this));
+	NULL_CHECK_RETURN(m_pTransform, E_FAIL);
+	m_uMapComponent[ID_DYNAMIC].insert({ L"Rigid", pComponent });
+
+	m_pRigid->m_bUseGrivaty = false;
+	m_pRigid->m_bFreezePos_X = true;
+	m_pRigid->m_bFreezePos_Y = true;
+	m_pRigid->m_bFreezePos_Z = true;
+
+	pComponent = m_pTextureCom = dynamic_cast<CTexture*>(Engine::Clone_Proto(L"Texture_Cube",this));
+	NULL_CHECK_RETURN(m_pTextureCom, E_FAIL);
+	m_uMapComponent[ID_STATIC].insert({ L"Texture_Cube", pComponent });
+
+	int a = 0;
+
+	pComponent = m_pCollider = dynamic_cast<CCollider*>(Engine::Clone_Proto(L"Collider", this));
+	NULL_CHECK_RETURN(m_pCollider, E_FAIL);
+	m_uMapComponent[ID_DYNAMIC].insert({ L"Collider", pComponent });
+	m_pCollider->Set_BoundingBox({ 2.f,2.f,2.f });
+	
+	return S_OK;
+}
+
+CCube * CCube::Create(LPDIRECT3DDEVICE9 pGraphicDev)
+{
+	CCube*		pInstance = new CCube(pGraphicDev);
+
+	if (FAILED(pInstance->Ready_GameObject()))
+	{
+		Safe_Release(pInstance);
+		return nullptr;
+	}
+
+	return pInstance;
+}
+
+void CCube::Free(void)
+{
+	__super::Free();
+}
--- Client/Stage.cpp
+++ Client/Stage.cpp
@@ -5,6 +5,7 @@
 #include "Monster.h"
 #include "Terrain.h"
 #include "SkyBox.h"
+#include "Cube.h"
 
 
 CStage::CStage(LPDIRECT3DDEVICE9 pGraphicDev)
@@ -19,7 +20,6 @@ CStage::~CStage()
 
 HRESULT CStage::Ready_Scene(void)
 {
-
 	FAILED_CHECK_RETURN(Ready_Layer_Environment(L"Layer_Environment"), E_FAIL);
 	FAILED_CHECK_RETURN(Ready_Layer_GameLogic(L"Layer_GameLogic"), E_FAIL);
 	FAILED_CHECK_RETURN(Ready_Layer_UI(L"Layer_UI"), E_FAIL);
@@ -58,9 +58,9 @@ HRESULT CStage::Ready_Layer_Environment(const _tchar* pLayerTag)
 	FAILED_CHECK_RETURN(pLayer->Add_GameObject(L"DynamicCamera", pGameObject), E_FAIL);*/
 	
 	// Terrain
-	pGameObject = CTerrain::Create(m_pGraphicDev);
+	/*pGameObject = CTerrain::Create(m_pGraphicDev);
 	NULL_CHECK_RETURN(pGameObject, E_FAIL);
-	FAILED_CHECK_RETURN(pLayer->Add_GameObject(L"Terrain", pGameObject), E_FAIL);
+	FAILED_CHECK_RETURN(pLayer->Add_GameObject(L"Terrain", pGameObject), E_FAIL);*/
 
 	pGameObject = CSkyBox::Create(m_pGraphicDev);
 	NULL_CHECK_RETURN(pGameObject, E_FAIL);
@@ -86,11 +86,68 @@ HRESULT CStage::Ready_Layer_GameLogic(const _tchar * pLayerTag)
 	NULL_CHECK_RETURN(pGameObject, E_FAIL);
 	FAILED_CHECK_RETURN(pLayer->Add_GameObject(L"Player", pGameObject), E_FAIL);
 
-	/*pGameObject = CMonster::Create(m_pGraphicDev);
-	NULL_CHECK_RETURN(pGameObject, E_FAIL);
-	FAILED_CHECK_RETURN(pLayer->Add_GameObject(L"Monster", pGameObject), E_FAIL);*/
+	int cubeCnt=0;
+	for (int i = 0; i < 20; i++)
+	{
+		for (int j = 0; j < 25; j++)
+		{
+			//맨 윗줄
+			if (i == 0)
+			{
+				TCHAR objName[128] = { 0 };
+				_stprintf_s(objName, _T("Map_Cube_%d"), (cubeCnt));
+				pGameObject = CCube::Create(m_pGraphicDev);
+				pGameObject->m_pTransform->m_vInfo[INFO_POS] = _vec3{ (float)j*1.99f,(float)i*1.99f,10.f };
+				NULL_CHECK_RETURN(pGameObject, E_FAIL);
+				FAILED_CHECK_RETURN(pLayer->Add_GameObject(objName, pGameObject), E_FAIL);
+				cubeCnt++;
+			}
 
+			//사이 첫줄
+			if (i == 19)
+			{
+				TCHAR objName[128] = { 0 };
+				_stprintf_s(objName, _T("Map_Cube_%d"), (cubeCnt));
+				pGameObject = CCube::Create(m_pGraphicDev);
+				pGameObject->m_pTransform->m_vInfo[INFO_POS] = _vec3{ (float)j*1.99f,(float)i*1.99f,10.f };
+				NULL_CHECK_RETURN(pGameObject, E_FAIL);
+				FAILED_CHECK_RETURN(pLayer->Add_GameObject(objName, pGameObject), E_FAIL);
+				cubeCnt++;
+			}
 
+			//사이 마지막줄
+			if (j == 0)
+			{
+				TCHAR objName[128] = { 0 };
+				_stprintf_s(objName, _T("Map_Cube_%d"), (cubeCnt));
+				pGameObject = CCube::Create(m_pGraphicDev);
+				pGameObject->m_pTransform->m_vInfo[INFO_POS] = _vec3{ (float)j*1.99f,(float)i*1.99f,10.f };
+				NULL_CHECK_RETURN(pGameObject, E_FAIL);
+				FAILED_CHECK_RETURN(pLayer->Add_GameObject(objName, pGameObject), E_FAIL);
+				cubeCnt++;
+			}
+
+			//맨 아랫줄
+			if (j == 24)
+			{
+				TCHAR objName[128] = { 0 };
+				_stprintf_s(objName, _T("Map_Cube_%d"), (cubeCnt));
+				pGameObject = CCube::Create(m_pGraphicDev);
+				pGameObject->m_pTransform->m_vInfo[INFO_POS] = _vec3{ (float)j*1.99f,(float)i*1.99f,10.f };
+				NULL_CHECK_RETURN(pGameObject, E_FAIL);
+				FAILED_CHECK_RETURN(pLayer->Add_GameObject(objName, pGameObject), E_FAIL);
+				cubeCnt++;
+			}
+
+			/*TCHAR objName[128] = {0};
+			_stprintf_s(objName, _T("Map_Cube_%d"), (cubeCnt));
+			pGameObject = CCube::Create(m_pGraphicDev);
+			pGameObject->m_pTransform->m_vInfo[INFO_POS] = _vec3{(float)j*1.99f,(float)i*1.99f,11.f };
+			NULL_CHECK_RETURN(pGameObject, E_FAIL);
+			FAILED_CHECK_RETURN(pLayer->Add_GameObject(objName, pGameObject), E_FAIL);
+			cubeCnt++;*/
+		}
+	}
 	m_uMapLayer.insert({ pLayerTag, pLayer });
 	return S_OK;
 }
