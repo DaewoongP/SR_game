52fe94895a50653ea487a86756b46dd8924f1a10
DaewoongP <ajdnd1432@gmail.com>
2023-04-01 18:06:03 +0900

    큐브 개수 카운트값 define

--- Engine/CollisionMgr.h
+++ Engine/CollisionMgr.h
@@ -15,12 +15,15 @@ public:
 	void		Add_Collider(CCollider* pCollider);
 	void		Check_Collision();
 
+	_bool		Collision_Range(CCollider* pSrc, CCollider* pDest);
 	_bool		Collision_Box(CCollider* pSrc, CCollider* pDest);
 	_bool		Check_BoundingBox(CCollider * pSrc, CCollider * pDest, _float * pX, _float * pY, _float * pZ);
 
 	void		Clear_Collision();
 private:
 	vector<CCollider*>	m_ColliderList;
+	// 충돌 감지 범위 설정 (콜라이더 박스중 가장 큰 길이에 곱해주어 사용함)
+	_float				m_fRangeOffset;
 private:
 	virtual void Free(void) override;
 };
