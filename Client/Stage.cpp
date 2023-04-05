a5e764e155fdf1827f9f12356e8feb3978beb4ef
shortusernam <122694993+shortusernam@users.noreply.github.com>
2023-04-05 22:20:49 +0900

    Topic key (#24)
    * Add Key
    
    * Key
    
    * Add Keycount
    
    ---------
    
    Co-authored-by: DaewoongP <38070554+DaewoongP@users.noreply.github.com>

--- Client/Stage.cpp
+++ Client/Stage.cpp
@@ -12,6 +12,7 @@
 #include "Pig.h"
 #include "PigTail.h"
 #include "Bat.h"
+#include"Key.h"
 
 CStage::CStage(LPDIRECT3DDEVICE9 pGraphicDev)
 	: CScene(pGraphicDev)
@@ -78,7 +79,12 @@ HRESULT CStage::Ready_Layer_GameLogic(const _tchar * pLayerTag)
 	//Pig
 	pGameObject = CPig::Create(m_pGraphicDev, pLayer);
 	NULL_CHECK_RETURN(pGameObject, E_FAIL);
-	FAILED_CHECK_RETURN(pLayer->Add_GameObject(L"Pig_0", pGameObject), E_FAIL);
+  FAILED_CHECK_RETURN(pLayer->Add_GameObject(L"Pig_0", pGameObject), E_FAIL);
+  
+	//Key
+	pGameObject = CKey::Create(m_pGraphicDev);
+	NULL_CHECK_RETURN(pGameObject, E_FAIL);
+	FAILED_CHECK_RETURN(pLayer->Add_GameObject(L"Key", pGameObject), E_FAIL);
 
 	// PLAYER
 	pGameObject = CPlayer::Create(m_pGraphicDev);
