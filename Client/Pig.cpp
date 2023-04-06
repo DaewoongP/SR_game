1ab6795a48dfcb65429f394db51ddd6ba7b41bf8
DaewoongP <ajdnd1432@gmail.com>
2023-04-06 17:27:51 +0900

    merge pig

--- Client/Pig.cpp
+++ Client/Pig.cpp
@@ -30,59 +30,8 @@ _int CPig::Update_GameObject(const _float & fTimeDelta)
 	Engine::Add_RenderGroup(RENDER_ALPHA, this);
 	m_pTextureCom->Update_Anim(fTimeDelta);
 	m_pTextureCom_Back->Update_Anim(fTimeDelta);
+	__super::Update_GameObject(fTimeDelta);
 	return 0;
-}
-
-void CPig::LateUpdate_GameObject(void)
-{
-	__super::LateUpdate_GameObject();
-}
-
-void CPig::Render_GameObject(void)
-{
-	m_pGraphicDev->SetTransform(D3DTS_WORLD, m_pTransform->Get_WorldMatrixPointer());
-
-	m_pTextureCom->Set_Texture(0);
-
-	m_pBufferCom->Render_Buffer();
-
-	__super::Render_GameObject();
-}
-
-HRESULT CPig::Add_Component(void)
-{
-	CComponent*		pComponent = nullptr;
-
-	pComponent = m_pBufferCom = dynamic_cast<CRcTex*>(Engine::Clone_Proto(L"RcTex", this));
-	NULL_CHECK_RETURN(m_pBufferCom, E_FAIL);
-	m_uMapComponent[ID_STATIC].insert({ L"RcTex", pComponent });
-	
-	pComponent = m_pTextureCom_Back = dynamic_cast<CTexture*>(Engine::Clone_Proto(L"Back_Pig_Texture", this));
-	NULL_CHECK_RETURN(m_pTextureCom_Back, E_FAIL);
-	m_uMapComponent[ID_STATIC].insert({ L"Back_Pig_Texture", pComponent });
-	m_pTextureCom_Back->Add_Anim(L"Idle", 0, 8, 1.f, true);
-	m_pTextureCom_Back->Switch_Anim(L"Idle");
-	m_pTextureCom_Back->m_bUseFrameAnimation = true;
-
-	pComponent = m_pTextureCom = dynamic_cast<CTexture*>(Engine::Clone_Proto(L"Pig_Texture", this));
-	NULL_CHECK_RETURN(m_pTextureCom, E_FAIL);
-	m_uMapComponent[ID_STATIC].insert({ L"Pig_Texture", pComponent });
-	m_pTextureCom->Add_Anim(L"Idle", 0, 8, 1.f, true);
-	m_pTextureCom->Switch_Anim(L"Idle");
-	m_pTextureCom->m_bUseFrameAnimation = true;
-
-	pComponent = m_pRigid = dynamic_cast<CRigidbody*>(Engine::Clone_Proto(L"Rigidbody", this));
-	NULL_CHECK_RETURN(m_pRigid, E_FAIL);
-	m_uMapComponent[ID_DYNAMIC].insert({ L"Rigidbody", pComponent });
-
-	pComponent = m_pCollider = dynamic_cast<CCollider*>(Engine::Clone_Proto(L"Collider", this));
-	NULL_CHECK_RETURN(m_pCollider, E_FAIL);
-	m_uMapComponent[ID_DYNAMIC].insert({ L"Collider", pComponent });
-	m_pCollider->Set_BoundingBox({ 1.0f, 1.0f, 0.1f });
-
-	m_pTransform->m_bIsStatic = false;
-
-	return S_OK;
 }
 
 _int CPig::Update_Too(const _float & fTimeDelta)
@@ -91,12 +40,10 @@ _int CPig::Update_Too(const _float & fTimeDelta)
 	m_pTransform->m_vInfo[INFO_POS].z = 10.f;
 	m_bBackSprite = false;
 
-
 	_vec3 MoveDir = { 1.0f,0.0f,0.0f };
 
 	m_pRigid->m_bUseGrivaty = true;
 
-	//�������� ��
 	if (-1.0f > m_pRigid->m_Velocity.y)
 	{
 		if (-5.0f > m_pRigid->m_Velocity.y)
@@ -160,8 +107,6 @@ _int CPig::Update_Top(const _float & fTimeDelta)
 
 	pPlayerTransformCom->Get_Info(INFO_POS, &vPlayerPos);
 
-	//�÷��̾ ���� �ִ��� �Ʒ� �ִ���
-	//�÷��̾ ��
 	if (vPlayerPos.y > m_pTransform->Get_WorldMatrixPointer()->_42)
 	{
 		m_bBackSprite = true;
@@ -182,10 +127,13 @@ _int CPig::Update_Top(const _float & fTimeDelta)
 	D3DXVec3Normalize(&vPlayerPos, &vPlayerPos);
 
 	m_pTransform->Move_Pos(&vPlayerPos, fTimeDelta, m_fSpeed);
-
-	Engine::Add_RenderGroup(RENDER_ALPHA, this);
 	
 	return 0;
+}
+
+void CPig::LateUpdate_GameObject(void)
+{
+	__super::LateUpdate_GameObject();
 }
 
 void CPig::LateUpdate_Too()
@@ -197,28 +145,22 @@ void CPig::LateUpdate_Top()
 	
 }
 
+void CPig::Render_GameObject(void)
+{
+	__super::Render_GameObject();
+}
+
 void CPig::Render_Too()
 {
-	m_pGraphicDev->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
-	m_pGraphicDev->SetRenderState(D3DRS_ALPHATESTENABLE, TRUE);
-
 	m_pGraphicDev->SetTransform(D3DTS_WORLD, m_pTransform->Get_WorldMatrixPointer());
 
 	m_pTextureCom->Set_Texture(0);
 
 	m_pBufferCom->Render_Buffer();
-
-	m_pGraphicDev->SetRenderState(D3DRS_ALPHATESTENABLE, FALSE);
-	m_pGraphicDev->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);
-
 }
 
 void CPig::Render_Top()
 {
-	//render
-	m_pGraphicDev->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
-	m_pGraphicDev->SetRenderState(D3DRS_ALPHATESTENABLE, TRUE);
-
 	m_pGraphicDev->SetTransform(D3DTS_WORLD, m_pTransform->Get_WorldMatrixPointer());
 
 	if (m_bBackSprite)
@@ -229,9 +171,6 @@ void CPig::Render_Top()
 		m_pTextureCom->Set_Texture(0);
 
 	m_pBufferCom->Render_Buffer();
-
-	m_pGraphicDev->SetRenderState(D3DRS_ALPHATESTENABLE, FALSE);
-	m_pGraphicDev->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);
 }
 
 void CPig::OnCollisionEnter(const Collision * collision)
@@ -258,7 +197,44 @@ void CPig::OnCollisionStay(const Collision * collision)
 	__super::OnCollisionStay(collision);
 }
 
-CPig * CPig::Create(LPDIRECT3DDEVICE9 pGraphicDev, CLayer* pLayer)
+HRESULT CPig::Add_Component(void)
+{
+	CComponent*		pComponent = nullptr;
+
+	pComponent = m_pBufferCom = dynamic_cast<CRcTex*>(Engine::Clone_Proto(L"RcTex", this));
+	NULL_CHECK_RETURN(m_pBufferCom, E_FAIL);
+	m_uMapComponent[ID_STATIC].insert({ L"RcTex", pComponent });
+
+	pComponent = m_pTextureCom_Back = dynamic_cast<CTexture*>(Engine::Clone_Proto(L"Back_Pig_Texture", this));
+	NULL_CHECK_RETURN(m_pTextureCom_Back, E_FAIL);
+	m_uMapComponent[ID_STATIC].insert({ L"Back_Pig_Texture", pComponent });
+	m_pTextureCom_Back->Add_Anim(L"Idle", 0, 8, 1.f, true);
+	m_pTextureCom_Back->Switch_Anim(L"Idle");
+	m_pTextureCom_Back->m_bUseFrameAnimation = true;
+
+	pComponent = m_pTextureCom = dynamic_cast<CTexture*>(Engine::Clone_Proto(L"Pig_Texture", this));
+	NULL_CHECK_RETURN(m_pTextureCom, E_FAIL);
+	m_uMapComponent[ID_STATIC].insert({ L"Pig_Texture", pComponent });
+	m_pTextureCom->Add_Anim(L"Idle", 0, 8, 1.f, true);
+	m_pTextureCom->Switch_Anim(L"Idle");
+	m_pTextureCom->m_bUseFrameAnimation = true;
+
+	pComponent = m_pRigid = dynamic_cast<CRigidbody*>(Engine::Clone_Proto(L"Rigidbody", this));
+	NULL_CHECK_RETURN(m_pRigid, E_FAIL);
+	m_uMapComponent[ID_DYNAMIC].insert({ L"Rigidbody", pComponent });
+
+	pComponent = m_pCollider = dynamic_cast<CCollider*>(Engine::Clone_Proto(L"Collider", this));
+	NULL_CHECK_RETURN(m_pCollider, E_FAIL);
+	m_uMapComponent[ID_DYNAMIC].insert({ L"Collider", pComponent });
+	m_pCollider->Set_BoundingBox({ 2.0f, 2.f, 0.1f });
+
+	m_pTransform->m_bIsStatic = false;
+
+	return S_OK;
+}
+
+
+CPig * CPig::Create(LPDIRECT3DDEVICE9 pGraphicDev)
 {
 	CPig*		pInstance = new CPig(pGraphicDev);
 
