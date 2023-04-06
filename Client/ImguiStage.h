9e13033114339239abc5a0eb768e9e93298902fc
duoface88 <72694443+duoface88@users.noreply.github.com>
2023-04-06 17:34:00 +0900

    Topic maptool imgui claer (#31)
    * imgui 내부 클래스화 및 정리
    
    기능(스테이지, 유닛 등) 툴 별로 window 창을 새로 띄우는 식으로 작동
    
    imguimgr 싱글톤으로 선언
    
    imgui stage tool 을 만들어 여기서 그리드, 큐브를 관리하도록 만듬
    
    유닛 툴 추가 예정
    
    이제 마우스 클릭으로 큐브 설치 가능
    ->cube placed 칸 활성화 후 마우스 좌클릭
    
    * imgui 메모리 누수 해결
    
    내용 없음
    
    * imgui Unit툴 원형 제작
    
    틀만 만들어 놓고 오브젝트랑 연동은 아직 안함
    
    * imgui unit 툴 몬스터, 맵 오브젝트 부분 원형 구현
    
    틀만 만들어 놓음
    
    ---------
    
    Co-authored-by: DaewoongP <38070554+DaewoongP@users.noreply.github.com>

---
+++ Client/ImguiStage.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "Include.h"
+
+class CImguiStage
+{
+public:
+	explicit CImguiStage(LPDIRECT3DDEVICE9 pGraphicDev);
+	~CImguiStage();
+
+public:
+	_int Update_Imgui_Stage();
+	void Release();
+
+private:
+	LPDIRECT3DDEVICE9 m_pGraphicDev;
+
+	// 큐브 저장을 위한 구조체
+	typedef struct CubeInfo
+	{
+		_vec3	vCubePos;					// 큐브 위치
+		int		iCubeTextureNumber;			// 큐브 스타일 번호
+	}CUBEINFO;
+
+	// 그리드 관련
+	vector<CGameObject*> m_vecGroundGrid;	// 그리드 모음
+	bool m_bGridON;							// 그리드 on / off
+	bool m_bGridCreate;						// 최초 그리드 생성
+
+	// 큐브 관련
+	CGameObject* m_pDefaultCube;			// 디폴트 큐브
+	vector<CUBEINFO> m_vecCubeInfo;			// 큐브 모음(저장 용도)
+
+	bool m_bCubePlaced;						// 디폴트 큐브 생성 
+	int m_iCubeTextureNumber;				// 큐브 스타일 번호
+	int m_iCubeIndex;						// 설치된 큐브 개수
+
+private:
+	// 그리드 메뉴
+	HRESULT GridMeun();
+	HRESULT GroundGridON(vector<CGameObject*>& vecGrid);
+
+	// 큐브 메뉴
+	HRESULT CubeMeun();
+	CGameObject* CreateDefaultCube();
+	void	CubeInstall(CGameObject* pDefaultCube, vector<CUBEINFO>& vecCubeInfo, int& iCubeIndex, int iCubeTextureNumber);
+
+	HRESULT SaveData(vector<CUBEINFO>& vecCubeInfo);
+	HRESULT LoadData(vector<CUBEINFO>& vecCubeInfo, int& iCubeIndex);
+	
+public:
+	static	CImguiStage* Create(LPDIRECT3DDEVICE9 pGraphicDev);
+};
+
