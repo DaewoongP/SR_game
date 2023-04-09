b6d0578c99ac19e2f38d2287c19189486720d51d
shortusernam <wjdghks718@gmail.com>
2023-04-09 20:33:55 +0900

    dll 추가

--- Client/Loading.cpp
+++ Client/Loading.cpp
@@ -55,8 +55,8 @@ HRESULT CLoading::Ready_Loading(LOADINGID eID)
 _uint CLoading::Loading_ForStage(void)
 {
 	//Sound 로딩오래걸릴경우 여기를 끄면됨
-	//Set_String(L"Sound/Bgm Loading..........");
-	//FAILED_CHECK_RETURN(Engine::Ready_Sound(), E_FAIL);
+	Set_String(L"Sound/Bgm Loading..........");
+	FAILED_CHECK_RETURN(Engine::Ready_Sound(), E_FAIL);
 	Engine::PlayBGM(L"9.wav", 0.5f);
 	Set_String(L"Sound/Effect Loading..........");
 	
