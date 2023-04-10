dbc0d7e187a4421fc849608d82b0ba7890cafb64
DaewoongP <ajdnd1432@gmail.com>
2023-04-10 18:14:27 +0900

    바닥 임시

--- Client/Stage1.cpp
+++ Client/Stage1.cpp
@@ -80,8 +80,26 @@ HRESULT CStage1::Ready_Layer_GameLogic(const _tchar * pLayerTag)
 
 	FAILED_CHECK_RETURN(FACTORY<CToodee>::Create(L"Toodee", pLayer, _vec3(10.f, 7.f, 10.f)), E_FAIL);
 	FAILED_CHECK_RETURN(FACTORY<CTopdee>::Create(L"Topdee", pLayer, _vec3(16.f, 10.f, 11.f)), E_FAIL);
-	
-	FAILED_CHECK_RETURN(FACTORY<CCube>::Create(L"MapCube", pLayer, _vec3(2.f, 2.f, 10.f)), E_FAIL);
+	int cubeCnt = 0;
+	for (int i = 0; i < CUBEY; i++)
+	{
+		for (int j = 0; j < CUBEX; j++)
+		{
+			//맨 윗줄
+			if (i == 0)
+				FAILED_CHECK_RETURN(FACTORY<CCube>::Create(L"MapCube", pLayer, _vec3{ (float)j * 2,(float)i * 2,10.f }), E_FAIL);
+			//사이 첫줄
+			if (i == CUBEY - 1)
+				FAILED_CHECK_RETURN(FACTORY<CCube>::Create(L"MapCube", pLayer, _vec3{ (float)j * 2,(float)i * 2,10.f }), E_FAIL);
+			//사이 마지막줄
+			if (j == 0)
+				FAILED_CHECK_RETURN(FACTORY<CCube>::Create(L"MapCube", pLayer, _vec3{ (float)j * 2,(float)i * 2,10.f }), E_FAIL);
+				cubeCnt++;
+			//맨 아랫줄
+			if (j == CUBEX - 1)
+				FAILED_CHECK_RETURN(FACTORY<CCube>::Create(L"MapCube", pLayer, _vec3{ (float)j * 2,(float)i * 2,10.f }), E_FAIL);
+		}
+	}
 
 	m_uMapLayer.insert({ pLayerTag, pLayer });
 
