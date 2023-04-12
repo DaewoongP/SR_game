a6685376e8d12531cecf1314df0c6e0e5a29ae3c
DaewoongP <ajdnd1432@gmail.com>
2023-04-12 14:09:47 +0900

    합병

--- Client/Stage1.cpp
+++ Client/Stage1.cpp
@@ -87,8 +87,6 @@ HRESULT CStage1::Ready_Layer_GameLogic(const _tchar * pLayerTag)
 	FAILED_CHECK_RETURN(FACTORY<CToodee>::Create(L"Toodee", pLayer, _vec3(58.f, 28.f, 10.f)), E_FAIL);
 	FAILED_CHECK_RETURN(FACTORY<CTopdee>::Create(L"Topdee", pLayer, _vec3(14.f, 32.f, 11.f)), E_FAIL);
 
-	FAILED_CHECK_RETURN(FACTORY<CBoss3>::Create(L"Boss3", pLayer, _vec3(30.f, 5.f, 10.f)), E_FAIL);
-
 	for (int i = 0; i < CUBEY; i++)
 	{
 		for (int j = 0; j < CUBEX; j++)
