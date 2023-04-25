96dc0dc994360a0fb611fd92ff4bc1664d297e0d
AnCheolMin <94519819+AnCheolMin@users.noreply.github.com>
2023-04-25 22:59:36 +0900

    레이져 탑디 오피격/투키 동기화
    레이져 탑디 오피격/투키 동기화

--- Client/Stage8.cpp
+++ Client/Stage8.cpp
@@ -98,7 +98,7 @@ HRESULT CStage8::Ready_Layer_Environment(const _tchar * pLayerTag)
 	CGameObject*		pGameObject = nullptr;
 
 	FAILED_CHECK_RETURN(FACTORY<CStage1Camera>::Create(L"Camera", pLayer), E_FAIL);
-	FAILED_CHECK_RETURN(FACTORY<CBoss3Camera>::Create(L"Boss3Camera", pLayer), E_FAIL);
+	//FAILED_CHECK_RETURN(FACTORY<CBoss3Camera>::Create(L"Boss3Camera", pLayer), E_FAIL);
 
 	FAILED_CHECK_RETURN(FACTORY<CStage1BG>::Create(L"StageBG", pLayer), E_FAIL);
 
