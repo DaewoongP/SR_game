a9ff65f624d869d446cc937a6ba95a7510e041b3
hyoenwoo <106013026+hyoenwoo@users.noreply.github.com>
2023-04-26 20:13:05 +0900

    보스 3연출

--- Client/Boss3DeadCamera.h
+++ Client/Boss3DeadCamera.h
@@ -1,11 +1,11 @@
 #pragma once
 #include "DirectCamera.h"
-class CBoss2DeadCamera :
+class CBoss3DeadCamera :
 	public CDirectCamera
 {
 private:
-	explicit CBoss2DeadCamera(LPDIRECT3DDEVICE9 pGraphicDev);
-	virtual ~CBoss2DeadCamera();
+	explicit CBoss3DeadCamera(LPDIRECT3DDEVICE9 pGraphicDev);
+	virtual ~CBoss3DeadCamera();
 	HRESULT Ready_Camera();
 	virtual _int Update_GameObject(const _float& fTimeDelta) override;
 
@@ -14,8 +14,8 @@ private:
 public:
 	void Set_BossDead(_bool bDead) { m_bBossDead = bDead; }
 public:
-	static CBoss2DeadCamera*		Create(LPDIRECT3DDEVICE9 pGraphicDev);
+	static CBoss3DeadCamera*		Create(LPDIRECT3DDEVICE9 pGraphicDev);
 private:
 	virtual void Free() override;
+	
 };
-
--- Client/Stage8.cpp
+++ Client/Stage8.cpp
@@ -19,7 +19,7 @@
 #include "Boss3Camera.h"
 #include "HpUI.h"
 #include "Fade.h"
-#include "BossDeadCamera.h"
+#include "Boss3DeadCamera.h"
 
 CStage8::CStage8(LPDIRECT3DDEVICE9 pGraphicDev)
 	:CScene(pGraphicDev),
@@ -97,8 +97,8 @@ HRESULT CStage8::Ready_Layer_Environment(const _tchar * pLayerTag)
 	CGameObject*		pGameObject = nullptr;
 
 	FAILED_CHECK_RETURN(FACTORY<CStage1Camera>::Create(L"Camera", pLayer), E_FAIL);
-	FAILED_CHECK_RETURN(FACTORY<CBoss2DeadCamera>::Create(L"BossDeadCamera", pLayer), E_FAIL);
-	FAILED_CHECK_RETURN(FACTORY<CBoss3Camera>::Create(L"Boss3Camera", pLayer), E_FAIL);
+	FAILED_CHECK_RETURN(FACTORY<CBoss3DeadCamera>::Create(L"Boss3DeadCamera", pLayer), E_FAIL);
+	//FAILED_CHECK_RETURN(FACTORY<CBoss3Camera>::Create(L"Boss3Camera", pLayer), E_FAIL);
 
 	FAILED_CHECK_RETURN(FACTORY<CStage1BG>::Create(L"StageBG", pLayer), E_FAIL);
 
@@ -223,7 +223,7 @@ void CStage8::PatternSet(const _float & fTimeDelta)
 		m_bUICheck = true;
 		m_iPreBossHp = 1;
 	}
-	if(1==iBossHp==0&&1==m_iPreBossHp)
+	if(iBossHp==0&&1==m_iPreBossHp)
 		m_bUICheck = true;
 
 }
