8d22ebabae4d48f4f5e0f5e5c92142ec1e7039a9
DaewoongP <ajdnd1432@gmail.com>
2023-04-26 17:00:30 +0900

    슈팅 플레이어

--- Client/ShootingPlayer.cpp
+++ Client/ShootingPlayer.cpp
@@ -17,27 +17,23 @@ CShootingPlayer::~CShootingPlayer()
 HRESULT CShootingPlayer::Ready_GameObject(_vec3 & vPos)
 {
 	FAILED_CHECK_RETURN(Add_Component(), E_FAIL);
-	__super::Ready_GameObject(vPos);
-
-
-
 
 	return S_OK;
 }
 
 _int CShootingPlayer::Update_GameObject(const _float & fTimeDelta)
 {
-	return _int();
+	return OBJ_NOEVENT;
 }
 
 _int CShootingPlayer::Update_Too(const _float & fTimeDelta)
 {
-	return _int();
+	return 0;
 }
 
 _int CShootingPlayer::Update_Top(const _float & fTimedDelte)
 {
-	return _int();
+	return 0;
 }
 
 void CShootingPlayer::LateUpdate_GameObject(void)
@@ -45,10 +41,6 @@ void CShootingPlayer::LateUpdate_GameObject(void)
 }
 
 void CShootingPlayer::Render_GameObject(void)
-{
-}
-
-void CShootingPlayer::SwapTrigger()
 {
 }
 
@@ -64,13 +56,14 @@ void CShootingPlayer::OnCollisionExit(const Collision * collision)
 {
 }
 
-void CShootingPlayer::Set_Die()
-{
-}
-
 void CShootingPlayer::Key_Input(const _float & fTimeDelta)
 {
-
+	if (Engine::Get_DIKeyState(DIK_LEFT) == Engine::KEYDOWN)
+	{
+	}
+	if (Engine::Get_DIKeyState(DIK_RIGHT) == Engine::KEYDOWN)
+	{
+	}
 }
 
 HRESULT CShootingPlayer::Add_Component(void)
@@ -81,16 +74,22 @@ HRESULT CShootingPlayer::Add_Component(void)
 	NULL_CHECK_RETURN(m_pBufferCom, E_FAIL);
 	m_vecComponent[ID_STATIC].push_back({ L"RcTex", pComponent });
 
-	pComponent = m_pTextureCom = dynamic_cast<CTexture*>(Engine::Clone_Proto(L"Yoga", this));
+	pComponent = m_pTextureCom = dynamic_cast<CTexture*>(Engine::Clone_Proto(L"ThirddeeShooting", this));
 	NULL_CHECK_RETURN(m_pTextureCom, E_FAIL);
-	m_vecComponent[ID_STATIC].push_back({ L"Yoga", pComponent });
+	m_vecComponent[ID_STATIC].push_back({ L"ThirddeeShooting", pComponent });
 
 	return S_OK;
 }
 
 CShootingPlayer * CShootingPlayer::Create(LPDIRECT3DDEVICE9 pGraphicDev, _vec3 & vPos)
 {
-	return nullptr;
+	CShootingPlayer* pInstance = new CShootingPlayer(pGraphicDev);
+	if (FAILED(pInstance->Ready_GameObject(vPos)))
+	{
+		Safe_Release(pInstance);
+		return nullptr;
+	}
+	return pInstance;
 }
 
 void CShootingPlayer::Free(void)
--- Client/ShootingPlayer.h
+++ Client/ShootingPlayer.h
@@ -21,18 +21,16 @@ public:
 	virtual _int Update_Top(const _float& fTimedDelte) override;
 	virtual void LateUpdate_GameObject(void) override;
 	virtual void Render_GameObject(void) override;
-	virtual void SwapTrigger();
 
 	virtual void OnCollisionEnter(const class Collision* collision);
 	virtual void OnCollisionStay(const class Collision* collision);
 	virtual void OnCollisionExit(const class Collision* collision);
 
-	void	Set_Die();
 	void		Key_Input(const _float& fTimeDelta);
+private:
 	HRESULT		Add_Component(void);
 
 private:
-	_float	m_fTopOffset;
 	Engine::CRcTex*			m_pBufferCom;
 	Engine::CTexture*		m_pTextureCom;
 
