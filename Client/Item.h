3e3538f05f02e720b549f780a57c4f7da687e389
duoface88 <micron826@naver.com>
2023-04-26 23:36:32 +0900

    아이템 4개
    아직 효과는 없음

---
+++ Client/Item.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "GameObject.h"
+
+BEGIN(Engine)
+
+class CRcTex;
+class CTexture;
+class CCollider;
+
+END
+
+class CItem : public CGameObject
+{
+private:
+	explicit CItem(LPDIRECT3DDEVICE9 pGraphicDev);
+	virtual ~CItem();
+
+public:
+	virtual HRESULT Ready_GameObject(_vec3& vPos, _int iIndex);
+	virtual _int Update_GameObject(const _float& fTimeDelta) override;
+	virtual void LateUpdate_GameObject(void) override;
+	virtual void Render_GameObject(void) override;
+
+	virtual void OnCollisionEnter(const class Collision* collision);
+
+private:
+	HRESULT		Add_Component(void);
+
+private:
+	Engine::CRcTex*			m_pBufferCom;
+	Engine::CTexture*		m_pTextureCom;
+	Engine::CCollider*		m_pCollider;
+
+	_int					m_iIndex;
+
+public:
+	static CItem*		Create(LPDIRECT3DDEVICE9 pGraphicDev, _vec3& vPos, _int iIndex);
+
+protected:
+	virtual void Free(void) override;
+};
+
